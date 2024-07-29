a55deb3e056000d47ed7c0c98e3c810680c511d6
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-07-29 13:28:37 +0300

    Add createPlayer factory function

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const gameBoard = (function () {
   //Defining the size of the game area
-  const rows = 3;
-  const columns = 3;
+  const _rows = 3;
+  const _columns = 3;
 
-  let gameBoard = Array(rows) 
+  let _gameBoard = Array(_rows)
     .fill()
-    .map(() => Array(columns).fill("")); //Create 2D Array
-  console.log(gameBoard);
+    .map(() => Array(_columns).fill("")); //Create 2D Array
+
+  console.log(_gameBoard); //Logging array
 
   //Setting a getter for gameBoard
-  const getGameBoard = () => gameBoard;
-  
+  const getGameBoard = () => _gameBoard;
+
   return {
-    getGameBoard
-  }
+    getGameBoard,
+  };
 })();
+
+const createPlayer = function (name) {
+  let score = 0; //Create private score variable for each player object
+  const getScore = () => score;
+  const giveScore = () => score++;
+
+  return { name, getScore, giveScore };
+}
+
+// const Player1 = createPlayer("Furkan");
+// const Player2 = createPlayer("Serkan");
